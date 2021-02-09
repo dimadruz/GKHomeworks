46b5c4993d3f38523a3c5460cb926077b2dff444
dimadruz <78649700+dimadruz@users.noreply.github.com>
2021-02-09 20:03:36 +0300

    Update Lesson1.java

--- Lesson1.java
+++ Lesson1.java
@@ -1,36 +1,7 @@
 public class Lesson1 {
     // Задание 1.
     public static void main(String[] args) {
-        // Задание 3
-        float a = 5;
-        float b = 4;
-        float c = 6;
-        float d = 7;
-        float val = bar(a = 5, b = 4, c = 6, d = 7);
-        bar(a, b, c, d);
-    }
 
-    // Задание 2.
-    public static void Main(String[] args) {
-
-        byte b = 4;
-        short s = 14;
-        int i = 322;
-        long l = 832L;
-        float f = 3.2f;
-        double d = 7.8;
-        char c = 123;
-        boolean bool = true;
-public class Lesson1 {
-    // Задание 1.
-    public static void main(String[] args) {
-        // Задание 3
-        float a = 5;
-        float b = 4;
-        float c = 6;
-        float d = 7;
-        float val = bar(a = 5, b = 4, c = 6, d = 7);
-        bar(a, b, c, d);
     }
 
     // Задание 2.
@@ -91,6 +62,7 @@ public class Lesson1 {
     public static void seven(String Name) {
         System.out.println("Привет, Меня зовут Дима");
     }
-    
+
 
 }
+
